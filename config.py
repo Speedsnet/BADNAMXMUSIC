939b2ccb580cbb3f7c63f6aee88ba6d92ce03d0e
BADNAM07123 <kb7266411@gmail.com>
2024-11-17 18:25:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "hhttps://github.com/BADNAM07123/BADNAMXMUSIC",
+    "https://github.com/BADNAM07123/BADNAMXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
